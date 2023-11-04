b4e589892dfbf3849c7276482fc282ab62f03dd1
ArnabShikder24 <arnabshikder2001@gmail.com>
2023-11-04 22:29:47 +0600

    single product api implement by redux

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,21 +1,39 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import ProductReview from '@/components/ProductReview';
 import { Button } from '@/components/ui/button';
+import { toast } from '@/components/ui/use-toast';
+import { useGetProductQuery } from '@/redux/api/apiSlice';
+import { addToCart } from '@/redux/feature/cart/cartSlice';
+import { useAppDispatch } from '@/redux/hook';
 import { IProduct } from '@/types/globalTypes';
-import { useEffect, useState } from 'react';
+// import { IProduct } from '@/types/globalTypes';
+// import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function ProductDetails() {
   const { id } = useParams();
+  const { data: product, isLoading } = useGetProductQuery(id);
+  const dispatch = useAppDispatch();
+
+  if (isLoading) {
+    return <p className='mt-10 text-center'>Loading...</p>
+  }
 
   //! Temporary code, should be replaced with redux
-  const [data, setData] = useState<IProduct[]>([]);
-  useEffect(() => {
-    fetch('../../public/data.json')
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
+  // const [data, setData] = useState<IProduct[]>([]);
+  // useEffect(() => {
+  //   fetch('../../public/data.json')
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data));
+  // }, []);
 
-  const product = data?.find((item) => item._id === Number(id));
+  // const product = data?.find((item) => item._id === Number(id));
+  const handleAddProduct = (product: IProduct) => {
+    dispatch(addToCart(product))
+    toast({
+      description: 'Product Added',
+    });
+  };
 
   //! Temporary code ends here
 
@@ -29,11 +47,11 @@ export default function ProductDetails() {
           <h1 className="text-3xl font-semibold">{product?.name}</h1>
           <p className="text-xl">Rating: {product?.rating}</p>
           <ul className="space-y-1 text-lg">
-            {product?.features?.map((feature) => (
+            {product?.features?.map((feature: any) => (
               <li key={feature}>{feature}</li>
             ))}
           </ul>
-          <Button>Add to cart</Button>
+          <Button onClick={() => handleAddProduct(product)}>Add to cart</Button>
         </div>
       </div>
       <ProductReview />
