5677ee8a6f3d9acf0cb88a8bff3dbf7a5322a73f
ArnabShikder24 <arnabshikder2001@gmail.com>
2023-11-04 22:03:15 +0600

    useGetProductsQuery implemented

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -2,23 +2,29 @@ import ProductCard from '@/components/ProductCard';
 import { Label } from '@/components/ui/label';
 import { Slider } from '@/components/ui/slider';
 import { Switch } from '@/components/ui/switch';
-import { useToast } from '@/components/ui/use-toast';
+// import { useToast } from '@/components/ui/use-toast';
+import { useGetProductsQuery } from '@/redux/api/apiSlice';
 import { setPriceRange, toggleState } from '@/redux/feature/product/productSlice';
 import { useAppDispatch, useAppSelector } from '@/redux/hook';
 import { IProduct } from '@/types/globalTypes';
-import { useEffect, useState } from 'react';
+// import { IProduct } from '@/types/globalTypes';
+// import { useEffect, useState } from 'react';
 
 export default function Products() {
-  const [data, setData] = useState<IProduct[]>([]);
+  // const [data, setData] = useState<IProduct[]>([]);
   const { priceRange, status } = useAppSelector(state => state.product);
+  const { data, isLoading } = useGetProductsQuery(undefined);
   const dispatch = useAppDispatch();
-  useEffect(() => {
-    fetch('./data.json')
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
+  // useEffect(() => {
+  //   fetch('./data.json')
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data));
+  // }, []);
 
-  const { toast } = useToast();
+  // const { toast } = useToast();
+  if (isLoading) {
+    return <p className='mt-10 text-center'>Loading...</p>
+  }
 
   //! Dummy Data
 
@@ -35,13 +41,13 @@ export default function Products() {
   let productsData;
 
   if (status) {
-    productsData = data.filter(
-      (item) => item.status === true && item.price < priceRange
+    productsData = data?.data.filter(
+      (item: { status: boolean; price: number; }) => item.status === true && item.price < priceRange
     );
   } else if (priceRange > 0) {
-    productsData = data.filter((item) => item.price < priceRange);
+    productsData = data?.data.filter((item: { price: number; }) => item.price < priceRange);
   } else {
-    productsData = data;
+    productsData = data?.data;
   }
 
   return (
@@ -69,7 +75,7 @@ export default function Products() {
         </div>
       </div>
       <div className="col-span-9 grid grid-cols-3 gap-10 pb-20">
-        {productsData?.map((product) => (
+        {productsData?.map((product: IProduct) => (
           <ProductCard product={product} key={product._id} />
         ))}
       </div>
